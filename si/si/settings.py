b2051cef7c1673c6fae0b47d2e668c711d0cb098
Oussama SIYAGH <o.lions.siyagh@gmail.com>
2020-03-04 17:33:55 +0100

    loading static files

--- si/si/settings.py
+++ si/si/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app',
 ]
 
 MIDDLEWARE = [
@@ -118,5 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'))
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+                    )
+# static root
+STATIC_ROOT = os.path.join(BASE_DIR, 'root')
+
 ALLOWED_HOSTS = ['*']
