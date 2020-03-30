b06536cfb4b4eb690f5578731a1ccb3a928c2da2
Oussama SIYAGH <o.lions.siyagh@gmail.com>
2020-03-30 01:14:07 +0100

    Accessing Django Admin Site

--- si/app/models.py
+++ si/app/models.py
@@ -1,11 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
 class Contact(models.Model):
-    name        = models.CharField(max_length=20)
-    email       = models.emailField(max_length=100)
-    phone       = models.IntegerField()
-    info        = models.CharField(max_length=30)
-    gender      = models.CharField(max_length=50, choices=(('male', 'Male'),('female', 'Female')))
-    image       = models.ImageField(upload_to='images/', blank = True)
-    date_added  = models.DateField(auto_now_add=True)
+    name = models.CharField(max_length=20)
+    email = models.EmailField(max_length=100)
+    phone = models.IntegerField()
+    info = models.CharField(max_length=30)
+    gender = models.CharField(max_length=50, choices=(('male', 'Male'), ('female', 'Female')))
+    image = models.ImageField(upload_to='images/', blank=True)
+    date_added = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
