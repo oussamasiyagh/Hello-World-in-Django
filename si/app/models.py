d7681b3cb833dea1d81bfd8e6522d19837d7924c
Oussama SIYAGH <o.lions.siyagh@gmail.com>
2020-04-05 02:07:21 +0100

    passing data from template to views

--- si/app/models.py
+++ si/app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils.timezone import datetime
 
+
 # Create your models here.
 class Contact(models.Model):
     name = models.CharField(max_length=20)
--- si/app/views.py
+++ si/app/views.py
@@ -1,14 +1,39 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Contact
-# Create your views here.
-def home(request):
-    context ={
-        'contacts' : Contact.objects.all()
-    }
-    return render(request, 'index.html', context)
+from django.views.generic import ListView, DetailView
 
-def detail(request, id):
-    context ={
-        'contact' : get_object_or_404(Contact, pk=id)
-    }
-    return render(request, 'detail.html', context)
+
+# Create your views here.
+# def home(request):
+#    context ={
+#        'contacts' : Contact.objects.all()
+#    }
+#    return render(request, 'index.html', context)
+
+# def detail(request, id):
+#    context ={
+#        'contact' : get_object_or_404(Contact, pk=id)
+#    }
+#    return render(request, 'detail.html', context)
+
+class HomePageView(ListView):
+    template_name = 'index.html'
+    model = Contact
+    context_object_name = 'contacts'
+
+
+class ContactDetailView(DetailView):
+    template_name = 'detail.html'
+    model = Contact
+    context_object_name = 'contact'
+
+
+def search(request):
+    if request.GET:
+        search_term = request.GET['search_term']
+        context = {
+            'search_term': search_term
+        }
+        return render(request, 'search.html', context)
+    else:
+        return redirect('home')
