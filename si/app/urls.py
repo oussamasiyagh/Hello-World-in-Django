29ad763b437fa63302da0a6ba231451eb4a417ca
Oussama SIYAGH <o.lions.siyagh@gmail.com>
2020-04-05 08:52:46 +0100

    Updating works perfectly!

--- si/app/urls.py
+++ si/app/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     # path('detail/<int:id>', views.detail, name = "detail"),
     path('detail/<int:pk>', views.ContactDetailView.as_view(), name="detail"),
     path('search/', views.search, name="search"),
+    path('contacts/create', views.ContactCreateView.as_view(), name="create"),
+    path('contacts/update/<int:pk>', views.ContactUpdateView.as_view(), name="update"),
+
 ]
